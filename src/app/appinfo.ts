cf6482c3a259abbe839a3d042cab6b4355103a4b
tassun <32536248+tassun@users.noreply.github.com>
2024-08-03 13:41:30 +0700

    initialize project

---
+++ src/app/appinfo.ts
@@ -0,0 +1,46 @@
+var DEFAULT_LANGUAGE = process.env.VUE_APP_DEFAULT_LANGUAGE;
+var API_URL = process.env.VUE_APP_API_URL;
+var BASE_URL = process.env.VUE_APP_BASE_URL;
+var CDN_URL = process.env.VUE_APP_CDN_URL;
+var IMG_URL = process.env.VUE_APP_IMG_URL;
+var BASE_STORAGE = process.env.VUE_APP_BASE_STORAGE;
+var API_TOKEN = process.env.VUE_APP_API_TOKEN;
+var DEFAULT_RAW_PARAMETERS = process.env.VUE_APP_DEFAULT_RAW_PARAMETERS == "true";
+export const DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded; charset=UTF-8";
+console.info("DEFAULT_LANGUAGE="+DEFAULT_LANGUAGE,", BASE_STORAGE="+BASE_STORAGE,", DEFAULT_RAW_PARAMETERS="+DEFAULT_RAW_PARAMETERS);
+console.info("API_URL="+API_URL,", BASE_URL="+BASE_URL,", CDN_URL="+CDN_URL,", IMG_URL="+IMG_URL);
+console.info("API_TOKEN="+API_TOKEN);
+export function getDefaultLanguage() { return DEFAULT_LANGUAGE; }
+export function setDefaultLanguage(language: string) {
+	console.log("set default_language="+language);
+	if(language && language.trim().length>0) DEFAULT_LANGUAGE = language;
+}
+export function getApiToken() { return API_TOKEN; }
+export function getApiUrl() { return API_URL; }
+export function getBaseUrl() { return BASE_URL; }
+export function getCdnUrl() { return CDN_URL; }
+export function getImgUrl() { return IMG_URL; }
+export function getBaseStorage() { return BASE_STORAGE; }
+export function getDefaultRawParameters() { return DEFAULT_RAW_PARAMETERS; }
+export function setApiToken(value: string) { API_TOKEN = value; }
+export function setApiUrl(value: string) { API_URL = value; }
+export function setBaseUrl(value: string) { BASE_URL = value; }
+export function setCdnUrl(value: string) { CDN_URL = value; }
+export function setImgUrl(value: string) { IMG_URL = value; }
+export function setBaseStorage(value: string) { BASE_STORAGE = value; }
+export function setDefaultRawParameters(value: boolean) { DEFAULT_RAW_PARAMETERS = value; }
+
+var default_labels : Array<any> = [];
+var program_labels : Array<any> = [];
+var program_message : Array<any> = [];
+export function getProgramMessage() : Array<any> { return program_message; }
+export function getDefaultLabels() : Array<any>{ return default_labels; }
+export function getProgramLabels() : Array<any> { return program_labels; }
+export function setProgramMessage(message: Array<any>) { program_message = message; }
+export function setDefaultLabels(labels: Array<any>) { default_labels = labels; }
+export function setProgramLabels(labels: Array<any>) { program_labels = labels; }
+export function appInit(settings = {program_message,default_labels,program_labels}) {
+	setProgramMessage(settings.program_message);
+	setDefaultLabels(settings.default_labels);
+	setProgramLabels(settings.program_labels);
+}
---
+++ src/app/messenger.ts
@@ -0,0 +1,170 @@
+import { getApiUrl, getBaseUrl, getCdnUrl, getImgUrl, getDefaultLanguage, getApiToken, getBaseStorage, setApiUrl, setBaseUrl, setCdnUrl, setImgUrl, setDefaultLanguage, setApiToken, getDefaultRawParameters  } from "./appinfo.js";
+import { DH } from "./dh.js";
+
+var messagingCallback : Function | undefined;
+var currentWindow : any;
+export function setMessagingCallback(callback?: Function) {
+    messagingCallback = callback;
+}
+export function setCurrentWindow(curwin: any) {
+    currentWindow = curwin;
+}
+export function getCurrentWindow() { return currentWindow; }
+export function getStorage(key: string) {
+	if("local"==getBaseStorage()) {
+		return localStorage.getItem(key);
+	}
+    return sessionStorage.getItem(key);
+}
+export function setStorage(key: string, value: string) {
+	if("local"==getBaseStorage()) {
+		localStorage.setItem(key,value);
+		return;
+	}
+	sessionStorage.setItem(key,value);
+}
+export function removeStorage(key: string) {
+	if("local"==getBaseStorage()) {
+		localStorage.removeItem(key);
+		return;
+	}
+    sessionStorage.removeItem(key);
+}
+export function getAccessorInfo() {
+    let info = getStorage("accessorinfo");
+    if(info && info!="") {
+        try { return JSON.parse(info); } catch(ex) { console.error(ex); }
+    }    
+    return null;
+}
+export function getAccessorToken() {
+    let json = getAccessorInfo();
+    if(json && json.authtoken) {
+        return json.authtoken;
+    }
+    let token = getApiToken();
+	if(token && token!="") return token;
+    return "";
+}
+export function saveAccessorInfo(json: any) {
+	setStorage("accessorinfo",JSON.stringify(json));
+}
+export function removeAccessorInfo() {
+	removeStorage("accessorinfo");
+}
+export function sendMessageInterface(win: any) {
+    let moderator = win?"opener":"parent";
+	let info = getAccessorInfo();
+    let options = getStorage("accessoptions");
+	let msg = {type: "storage", moderator: moderator, API_URL: getApiUrl(), BASE_URL: getBaseUrl(), CDN_URL: getCdnUrl(), IMG_URL: getImgUrl(), DEFAULT_LANGUAGE: getDefaultLanguage(), API_TOKEN: getApiToken(), accessorinfo: info, accessoptions: options};
+	return sendMessageToFrame(msg,win);
+}
+export function sendMessageToFrame(data: any,win: any) {
+    if(!data) return false;
+    try {
+		console.log("sendMessageToFrame:",data);
+        if(!win) win = document.getElementsByTagName('iframe')[0].contentWindow;    
+        if(win) win.postMessage(JSON.stringify(data), "*");	
+        return true;
+    } catch(ex) { console.log(ex); }
+    return false;
+}
+export function requestAccessorInfo(callback?: Function) {
+    if(callback) setMessagingCallback(callback);
+    let msg = { type: "accessorinfo" };
+    console.log("requestAccessorInfo: ",msg);
+    console.log("window.opener",window.opener);
+    console.log("window.parent",window.parent);
+    if(window.opener) {
+        return sendMessageToOpener(msg);
+    }
+    return sendMessageToParent(msg);
+}
+export function sendMessageToParent(data: any) {
+    if(!data) return;
+    try {
+        console.log("sendMessageToParent:",data);
+        window.parent.postMessage(JSON.stringify(data), "*");
+        return true;
+    } catch(ex) { console.log(ex); }
+    return false;
+}
+export function sendMessageToOpener(data: any) {
+    if(!data) return;
+    try {
+        console.log("sendMessageToOpener:",data);
+        window.opener.postMessage(JSON.stringify(data), "*");
+        return true;
+    } catch(ex) { console.log(ex); }
+    return false;
+}
+export function handleRequestMessage(data: any) {
+    console.log("handleRequestMessage: data",data);
+    if(data.type=="storage") {
+        if(data.API_URL !== undefined) setApiUrl(data.API_URL);
+        if(data.BASE_URL !== undefined) setBaseUrl(data.BASE_URL);
+        if(data.CDN_URL !== undefined) setCdnUrl(data.CDN_URL);
+        if(data.IMG_URL !== undefined) setImgUrl(data.IMG_URL);
+        if(data.DEFAULT_LANGUAGE !== undefined) setDefaultLanguage(data.DEFAULT_LANGUAGE);
+        if(data.API_TOKEN !== undefined) setApiToken(data.API_TOKEN);
+        if(data.accessoptions !== undefined) setStorage("accessoptions",data.accessoptions);
+        if(data.accessorinfo) {
+            saveAccessorInfo(data.accessorinfo);
+        }
+        console.info("handleRequestMessage: accessor info",data.accessorinfo);
+        console.info("handleRequestMessage: DEFAULT_LANGUAGE="+getDefaultLanguage(),", BASE_STORAGE="+getBaseUrl(),", DEFAULT_RAW_PARAMETERS="+getDefaultRawParameters());
+        console.info("handleRequestMessage: API_URL="+getApiUrl(),", BASE_URL="+getBaseUrl(),", CDN_URL="+getCdnUrl(),", IMG_URL="+getImgUrl());
+        console.info("handleRequestMessage: API_TOKEN="+getApiToken());        
+    }
+    if(messagingCallback) messagingCallback(data);
+}
+export function setupDiffie(json: any) {
+	console.log("setupDiffie",getAccessorToken());
+    let info = json.body.info;
+    if(info) {
+        const dh = new DH();
+        dh.prime = info.prime;
+        dh.generator = info.generator;
+        dh.otherPublicKey = info.publickey;
+        dh.compute();
+        dh.updatePublicKey();
+        info.privatekey = dh.privateKey;
+        info.publickey = dh.publicKey;
+        info.sharedkey = dh.sharedKey;
+        info.otherpublickey = dh.otherPublicKey;
+        saveAccessorInfo(json.body);
+    }
+}
+export function getDH() {
+    let json = getAccessorInfo();
+    if(json && json.info) {
+        let info = json.info;
+        if(info.prime && info.generator && info.publickey && info.privatekey && info.sharedkey && info.otherpublickey) {
+            const dh = new DH();
+            dh.prime = info.prime;
+            dh.generator = info.generator;
+            dh.otherPublicKey = info.publickey;
+            dh.privateKey = info.privatekey;
+            dh.publicKey = info.publickey;
+            dh.sharedKey = info.sharedkey;
+            dh.otherPublicKey = info.otherpublickey;
+            return dh;
+        }
+    }
+    return null;
+}
+window.onmessage = function(e) {
+    console.log("window-messenger: onmessage:",e.data);
+    try {
+        let payload = e.data;
+        if(typeof payload === 'string') { payload = JSON.parse(e.data); }
+        //in case of parent window, try to send accessor info
+        /*
+        if(payload.type=="accessorinfo") {					
+            sendMessageInterface(getCurrentWindow());
+            return;
+        }*/
+        //in case of child window, try to handle request message
+        handleRequestMessage(payload);
+    } catch(ex) { console.error(ex); }
+}
